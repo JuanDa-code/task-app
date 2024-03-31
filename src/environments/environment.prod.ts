5d1bb20e1dcbc220d8b2064f299fe1e153a60df1
JuanDa-code <juandax123456@gmail.com>
2024-03-31 16:50:09 -0500

    Inicializacion del proyecto, vinculacion de firebase para el manejo de inicio de sesion, creacion de formulario de inicio de sesion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDcEBv5___Lx-1tICgT0eh0CEnVkBaP3m8",
+    authDomain: "gestor-tareas-c1eeb.firebaseapp.com",
+    projectId: "gestor-tareas-c1eeb",
+    storageBucket: "gestor-tareas-c1eeb.appspot.com",
+    messagingSenderId: "623256318518",
+    appId: "1:623256318518:web:41370742d3106755075e97",
+    measurementId: "G-B3W7WR4918"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDcEBv5___Lx-1tICgT0eh0CEnVkBaP3m8",
+    authDomain: "gestor-tareas-c1eeb.firebaseapp.com",
+    projectId: "gestor-tareas-c1eeb",
+    storageBucket: "gestor-tareas-c1eeb.appspot.com",
+    messagingSenderId: "623256318518",
+    appId: "1:623256318518:web:41370742d3106755075e97",
+    measurementId: "G-B3W7WR4918"
+  }
 };
 
 /*
