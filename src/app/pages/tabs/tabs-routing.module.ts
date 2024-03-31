e854efd6381f0c16aacde3357fe18a448bb4dc1d
JuanDa-code <juandax123456@gmail.com>
2024-03-31 18:05:19 -0500

    Registro de usuario, redireccion a pagina de tabs despues del inicio de sesion, actualizacion del nombre de usuario

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -7,6 +7,14 @@ const routes: Routes = [
   {
     path: '',
     component: TabsPage
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   }
 ];
 
